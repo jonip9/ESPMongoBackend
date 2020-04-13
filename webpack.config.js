2bfa3e061635c7af04a075d619ce761475798a4d
Joni <joni.possi@gmail.com>
2020-04-13 20:43:38 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './bin/index',
+    entry: './bin/www',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
